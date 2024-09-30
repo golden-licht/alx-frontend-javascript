5b8cee83d6e55845ac798232c60749be886a25fc
golden-licht <luminoonimul@gmail.com>
2024-09-30 19:53:05 +0300

    Just join?

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,15 +1,3 @@
 export default function iterateThroughObject (reportWithIterator) {
-  let result = '';
-  const { length } = reportWithIterator;
-
-  for (let idx in reportWithIterator) {
-    if (Object.hasOwn(reportWithIterator, idx)) {
-      idx = Number(idx);
-
-      result += idx !== length - 1
-        ? `${reportWithIterator[idx]} | `
-        : reportWithIterator[idx];
-    }
-  }
-  return result;
+  return reportWithIterator.join(' | ');
 }
