aae0b7e60a39dd9e665b2fdf71a615a5a3d97eac
golden-licht <luminoonimul@gmail.com>
2024-09-30 19:46:41 +0300

    For ... in instead?

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,12 +1,15 @@
-export default function iterateThroughObject(reportWithIterator) {
+export default function iterateThroughObject (reportWithIterator) {
   let result = '';
   const { length } = reportWithIterator;
 
-  for (let idx = 0; idx < length; idx += 1) {
-    result += idx !== length - 1
-      ? `${reportWithIterator[idx]} | `
-      : reportWithIterator[idx];
-  }
+  for (let idx in reportWithIterator) {
+    if (Object.hasOwn(reportWithIterator, idx)) {
+      idx = Number(idx);
 
+      result += idx !== length - 1
+        ? `${reportWithIterator[idx]} | `
+        : reportWithIterator[idx];
+    }
+  }
   return result;
 }
